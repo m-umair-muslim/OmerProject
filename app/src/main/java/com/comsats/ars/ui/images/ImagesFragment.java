b2d92c19fc845ce2839582dbc9ba6dd293eb205e
Umair Muslim <umair_muslim@yahoo.com>
2020-03-01 02:08:27 +0100

    Using ViewModel to get the images

--- app/src/main/java/com/comsats/ars/ui/images/ImagesFragment.java
+++ app/src/main/java/com/comsats/ars/ui/images/ImagesFragment.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
 import com.comsats.ars.R;
@@ -19,56 +21,26 @@ import com.google.firebase.storage.StorageReference;
 
 public class ImagesFragment extends Fragment {
 
-//    ImageView imageView;
-    //  private FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();
-    //private DatabaseReference databaseReference = firebaseDatabase.getReference();
-    // private DatabaseReference first = databaseReference.child("image");
-
-
     private ImagesViewModel galleryViewModel;
     private ImageView mImageView;
 
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        galleryViewModel =
-//                ViewModelProviders.of(this).get(ImagesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_images, container, false);
-//        final TextView textView = root.findViewById(R.id.text_gallery);
-//        galleryViewModel.getText().observe(this, new Observer<String>() {
-//
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//
-//        });
+        galleryViewModel = new ViewModelProvider(this).get(ImagesViewModel.class);
 
+        View root = inflater.inflate(R.layout.fragment_images, container, false);
         mImageView = root.findViewById(R.id.img);
+
+        galleryViewModel.getImageUri().observe(getViewLifecycleOwner(), new Observer<Uri>() {
+            @Override
+            public void onChanged(Uri uri) {
+                Glide.with(ImagesFragment.this)
+                        .load(uri)
+                        .into(mImageView);
+            }
+        });
 
         return root;
 
-
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        if (mImageView != null) {
-            StorageReference reference = FirebaseStorage.getInstance()
-                    .getReferenceFromUrl("gs://arsfirebaseproject-f638b.appspot.com/1logo.png");
-
-            reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-
-                    Glide.with(ImagesFragment.this)
-                            .load(uri)
-                            .into(mImageView);
-                }
-            });
-
-        }
     }
 }
--- app/src/main/java/com/comsats/ars/ui/images/ImagesViewModel.java
+++ app/src/main/java/com/comsats/ars/ui/images/ImagesViewModel.java
@@ -1,22 +1,35 @@
 package com.comsats.ars.ui.images;
 
+import android.net.Uri;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+
 public class ImagesViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<Uri> mImageRepo;
 
     public ImagesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
-
-
+        mImageRepo = new MutableLiveData<>();
+        //mText.setValue("This is gallery fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Uri> getImageUri() {
+        if (mImageRepo.getValue() == null) {
+            FirebaseStorage.getInstance()
+                    .getReferenceFromUrl("gs://arsfirebaseproject-f638b.appspot.com/1logo.png")
+                    .getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    mImageRepo.postValue(uri);
+                }
+            });
+        }
+        return mImageRepo;
     }
 
 }
